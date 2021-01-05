ccf6578e10b923eaa4dc00a940b8c55d4197f13b
ahuangzhi <1049729282@qq.com>
2021-01-05 15:57:02 +0800

    删除

--- src/views/traceability/systementityjs.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-const prefix = "besufile/";
-
-export function selectbues(){
-    return request({
-        url: prefix + 'list',
-        method: 'get'
-    })
-}
